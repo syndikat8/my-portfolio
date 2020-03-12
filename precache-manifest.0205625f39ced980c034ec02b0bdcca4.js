1e6cc82819468631c27a4df4851c9cdab4eeae00
Dima Matusik <k.syndikat@gmail.com>
2020-03-12 03:20:35 +0300

    Updates

---
+++ precache-manifest.0205625f39ced980c034ec02b0bdcca4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55c0f832e44e643aa6b25c7a124605eb",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "4c9105df99e57b1768fd",
+    "url": "/my-portfolio/static/css/main.e916549a.chunk.css"
+  },
+  {
+    "revision": "d5cad535b7dd7e67cf3b",
+    "url": "/my-portfolio/static/js/2.474a6b25.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my-portfolio/static/js/2.474a6b25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c9105df99e57b1768fd",
+    "url": "/my-portfolio/static/js/main.9e800e12.chunk.js"
+  },
+  {
+    "revision": "7c7d8426a5976bd5dcaf",
+    "url": "/my-portfolio/static/js/runtime-main.ec1bad13.js"
+  }
+]);
