6fdeb92534e43f84c2778b46434613b99c73f056
Dima Matusik <k.syndikat@gmail.com>
2020-04-15 16:56:01 +0300

    add CSS Contacts

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -6,11 +6,18 @@ function Contacts() {
     <div className={styles.contacts}>
       <div className={styles.container}>
         <h2> Контакты </h2>
+        <div className={styles.contactMain}>
+          <div className={styles.contactMainElement} >
+            <h3>Минск, БЕЛАРУСЬ</h3>
+            <a href="k.syndikat@gmail.com">k.syndikat@gmail.com</a>
+            <p>+375 29 2-64-13-78</p>
+          </div>
         <form className={styles.formatArea}>
           <input placeholder="ФИО" type="text"/>
           <input placeholder="Контакты" type="text"/>
           <textarea placeholder="Сообщение" ></textarea>
         </form>
+        </div>
         <button>Отправить</button>
       </div>
     </div>
