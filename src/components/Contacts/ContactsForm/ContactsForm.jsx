0149cd79f4072a13a0b7e31101784cc307647476
Dima Matusik <k.syndikat@gmail.com>
2020-06-14 20:08:05 +0300

    add form

--- src/components/Contacts/ContactsForm/ContactsForm.jsx
+++ src/components/Contacts/ContactsForm/ContactsForm.jsx
@@ -6,27 +6,27 @@ import {Field, reduxForm} from "redux-form";
 const ContactsForm = (props) => {
 
   return (
-            <form onSubmit={props.handleSubmit}  className={styles.formatArea}>
-              <div className={styles.formatAreaElement}>
-                <Field
-                  placeholder="Имя"
-                  name="name"
-                  component="input"/>
-                <Field
-                  placeholder="Контакты"
-                  name="contacts"
-                  component="input"/>
-              </div>
-              <Field
-                placeholder="Заголовок (необязательно)"
-                name="title"
-                component="input"/>
-              <Field
-                name="messages"
-                placeholder="Сообщение"
-                component="textarea"/>
-              <button>Отправить</button>
-            </form>
+    <form onSubmit={props.handleSubmit} className={styles.formatArea}>
+      <div className={styles.formatAreaElement}>
+        <Field
+          placeholder="Имя"
+          name="name"
+          component="input"/>
+        <Field
+          placeholder="Контакты"
+          name="contacts"
+          component="input"/>
+      </div>
+      <Field
+        placeholder="Заголовок (необязательно)"
+        name="title"
+        component="input"/>
+      <Field
+        name="messages"
+        placeholder="Сообщение"
+        component="textarea"/>
+      <button>Отправить</button>
+    </form>
   );
 }
 
