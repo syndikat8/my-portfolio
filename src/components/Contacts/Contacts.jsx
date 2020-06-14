f39239adfe3d3122f716bdac102b82fd9a95699d
Dima Matusik <k.syndikat@gmail.com>
2020-06-15 00:06:59 +0300

    add form

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -5,42 +5,55 @@ import ContactsFormRedux from "./ContactsForm/ContactsForm";
 import axios from "axios";
 
 
-const Contacts = () => {
+class Contacts extends React.Component {
 
-  const onSubmit = (formData) => {
-    debugger
-    console.log(formData)
-
-    axios.post("http://localhost:3005/sendMessage", {
-      name: formData.name,
-      contacts: formData.contacts,
-      title: formData.title,
-      messages: formData.messages,
-    }).
-    then(() => {
-      alert("Your message was")
-      }
-    )
-
+  state = {
+    behold: false
   }
 
-  return (
-    <div id="contacts" className={styles.contacts}>
-      <div className={styles.container}>
-        <h2> Контакты </h2>
-        <div className={styles.contactMain}>
-          <Fade left>
-            <div className={styles.contactMainElement}>
-              <h3>Минск, БЕЛАРУСЬ</h3>
-              <a href="https://mail.google.com/">k.syndikat@gmail.com</a>
-              <p>+375 29 26-41-378</p>
-            </div>
-            <ContactsFormRedux onSubmit={onSubmit}/>
-          </Fade>
+  render() {
+
+    const onSubmit = (formData) => {
+      axios.post("https://smpt-nodejs-server.herokuapp.com/sendMessage", {
+        name: formData.name,
+        contacts: formData.contacts,
+        title: formData.title,
+        messages: formData.messages,
+      }).then(() => {
+        this.setState({behold: true})
+        }
+      )
+    }
+
+    const onBeholdChange = () => {
+      this.setState({behold: false})
+    }
+
+    return (
+      <div id="contacts" className={styles.contacts}>
+        <div className={styles.container}>
+          <h2> Контакты </h2>
+          {this.state.behold &&
+            <span
+              onPointerEnter={onBeholdChange}
+              className={styles.messageForm}>"Cообщение получено! Скоро свяжусь с Вами:)"
+          </span>
+          }
+
+          <div className={styles.contactMain}>
+            <Fade left>
+              <div className={styles.contactMainElement}>
+                <h3>Минск, БЕЛАРУСЬ</h3>
+                <a href="https://mail.google.com/">k.syndikat@gmail.com</a>
+                <p>+375 29 26-41-378</p>
+              </div>
+              <ContactsFormRedux onSubmit={onSubmit}/>
+            </Fade>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Contacts;
